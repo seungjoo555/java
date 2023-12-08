9f556500a6946b9ac31ea23f15138b1886771ec7
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2023-12-08 17:49:53 +0900

    자바 1일차 : 테스트

---
+++ java_lsj/src/day01/TestEx1.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class TestEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//char ch1 = "A";
+		
+		//char ch1 = 'A';
+		//String ch1 = "A"; 
+	}
+
+}
---
+++ java_lsj/src/day01/TestEx2.java
@@ -0,0 +1,25 @@
+package day01;
+
+public class TestEx2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//국어성적
+		int i;
+		i = 53;
+		//영어성적
+		int j;
+		j = 66;
+		//수학성적
+		int k;
+		k = 90;
+		//성적 합산
+		double sum;
+		sum = i+j+k;
+		//평균
+		double evg;
+		evg = sum/3;
+		System.out.println(evg);
+	}
+
+}

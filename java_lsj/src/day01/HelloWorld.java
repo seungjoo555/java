1725e2ae236be952b7869c24bb7ac110c8c70502
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2023-12-08 14:25:05 +0900

    자바 1일차 : 샘플 코드 및 설명

---
+++ java_lsj/src/day01/HelloWorld.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class HelloWorld {
+
+	/**
+	 * 메소드를 설명할 때 사용하는 주석
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//sysout은 콘솔에 출력할 때 사용
+		System.out.println("Hello world!!!");
+	}
+
+}

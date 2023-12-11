91748d837cfe27a40dfed9a6f4969bd95453b8cb
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2023-12-11 17:28:06 +0900

    자바 2일차 : if ~ else문 예제

---
+++ java_lsj/src/day02/IfElseEx1.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class IfElseEx1 {
+
+	//if else 예제
+	public static void main(String[] args) {
+		//정수가 0이면 0이라고 출력하고, 아니면 0이 아닙니다라고 출력하는 예제
+		int num = 0;
+		
+		if(num == 0) {
+			System.out.println("num은 0입니다.");
+		}else {
+			System.out.println("num은 0이 아닙니다.");
+		}
+	}
+
+}

d8c325a1f21753071bead9130f1c20af7e84acb2
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2023-12-12 16:39:35 +0900

    자바 3일차 : 반복문으로 알파벳 출력

---
+++ java_lsj/src/day03/AlphabetEx1.java
@@ -0,0 +1,14 @@
+package day03;
+
+public class AlphabetEx1 {
+
+	public static void main(String[] args) {
+		/*
+		 * a 부터 z 까지 반복문을 이용하여 출력하는 코드를 작성하세요.
+		 */
+		for(char ch = 'a'; ch <= 'z'; ch++) {
+			System.out.print(ch + " ");
+		}
+	}
+
+}

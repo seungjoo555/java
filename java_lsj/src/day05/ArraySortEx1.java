4cc8fce4a5f36cc40c8187bc20b62635bbc99fe9
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2023-12-14 17:28:31 +0900

    자바 5일차 : 배열예제(정렬 방법)

--- java_lsj/src/day05/ArraySortEx1.java
+++ java_lsj/src/day05/ArraySortEx1.java
@@ -7,16 +7,22 @@ public class ArraySortEx1 {
 	//배열 예제
 	public static void main(String[] args) {
 		//배열 정렬 방법
-		int arr[] = new int[] {1, 3, 5, 7 ,2 , 4, 6, 8};
+		int arr[] = new int[] {1, 3, 5, 8, 2, 7, 6, 4};
 
 		//버블 정렬
 		/* 옆에 인접한 값들을 비교하여 정렬하는 방식
 		 * 1 3 5 7 2 4 6 8
-		 * 1 3 5 2 4 6 7 [8]
-		 * 1 3 2 4 5 6 [7 8]
+		 * 1 3 5 2 4 6 7 [8] -1번째 for문에서 i = 0
+		 * 1 3 2 4 5 6 [7 8] -1번째 for문에서 i = 1
+		 * 1 2 3 4 5 [6 7 8] -1번째 for문에서 i = 2
+		 * 1 2 3 4 [5 6 7 8] -1번째 for문에서 i = 3
+		 * 1 2 3 [4 5 6 7 8] -1번째 for문에서 i = 4
+		 * 1 2 [3 4 5 6 7 8] -1번째 for문에서 i = 5
+		 * 1 [2 3 4 5 6 7 8] -1번째 for문에서 i = 6
+		 * [1 2 3 4 5 6 7 8] -1번째 for문에서 i = 7
 		 */
 		for(int i = 0; i < arr.length-1; i++) {
-			for(int j = 0; j < arr.length-1; j++) {
+			for(int j = 0; j < arr.length-(1+i); j++) {
 				if(arr[j] > arr[j+1]) {
 					int tmp = arr[j];
 					arr[j] = arr[j+1];

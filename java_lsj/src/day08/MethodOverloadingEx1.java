a8e002ac454432af3e3f3ba2fd488d7484de454b
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2023-12-19 17:32:20 +0900

    자바 8일차 : 메서드 오버로딩

---
+++ java_lsj/src/day08/MethodOverloadingEx1.java
@@ -0,0 +1,35 @@
+package day08;
+
+public class MethodOverloadingEx1 {
+
+	public static void main(String[] args) {
+		System.out.println(sum(1, 2));
+		System.out.println(sum(1.2, 2.3));
+		System.out.println(sum(1.2, 2));
+		System.out.println(sum(1, 2, 3));
+		
+	}
+
+	//두 정수의 합을 알려주는 메서드
+	public static int sum(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	//두 실수의 합을 알려주는 메서드
+	public static double sum(double num1, double num2) {
+		return num1 + num2;
+	}
+	
+	//아래 메서드는 1번 메서드와 매개변수의 개수도 같고 자료형도 같기 때문에 메서드 오버로딩이 x
+	/*	리턴 타입이 다르다고 동작 되지 않는다.
+	public static double sum(int num1, int num2) {
+		return num1 + num2;
+	}
+	*/
+	
+	//세 정수의 합
+	public static int sum(int num1, int num2, int num3) {
+		return num1 + num2 + num3;
+	}
+	
+}

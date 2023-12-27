cf77cfb4455e9fef62b060b25e14fc7f17b5ef5e
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2023-12-27 17:33:59 +0900

    자바 13일차 : 캘린더 예제

---
+++ java_lsj/src/day13/CalendarEx1.java
@@ -0,0 +1,19 @@
+package day13;
+
+import java.util.Calendar;
+
+public class CalendarEx1 {
+
+	//캘린더 예제
+	public static void main(String[] args) {
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(Calendar.YEAR);
+		System.out.print(year + "년");
+		int month = cal.get(Calendar.MONTH) + 1;
+		System.out.print(month + "월");
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		System.out.print(day + "일");
+		
+	}
+
+}

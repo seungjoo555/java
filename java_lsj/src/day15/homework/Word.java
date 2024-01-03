48c81889f75fd1af8393f6b360de680579d1d9b3
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2024-01-03 14:21:04 +0900

    자바 15일차 : 숙제

--- java_lsj/src/day15/homework/Word.java
+++ java_lsj/src/day15/homework/Word.java
@@ -1,17 +1,14 @@
 package day15.homework;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class Word implements Serializable{
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 7166211807053272219L;
-	private ArrayList<String> list;
+public class Word{
+	
+	
+	
+	
+	
 }

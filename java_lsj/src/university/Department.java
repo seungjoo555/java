63671cfac7673b3fc35894d9437ab896e39e2f28
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2024-01-23 17:20:30 +0900

    수정

--- java_lsj/src/university/Department.java
+++ java_lsj/src/university/Department.java
@@ -8,19 +8,19 @@ import lombok.Data;
 // 학과 리스트
 @Data
 public class Department {
-/*과 클래스 // Department
-- 과 이름 (중복 x)
-- 정원 제한 (선택사항)
-- 학과 교수 (리스트)
-- 학생 (리스트)
-과를 만든후에 교수, 학생 추가
-교수 정원제한 학생 정원 제한을 입력받고 추가할때 사용
- */
+	/*과 클래스 // Department
+	- 과 이름 (중복 x)
+	- 정원 제한 (선택사항)
+	- 학과 교수 (리스트)
+	- 학생 (리스트)
+	과를 만든후에 교수, 학생 추가
+	교수 정원제한 학생 정원 제한을 입력받고 추가할때 사용
+	 */
 	private String name; //학과명 -중복 X
 	private List<Professor> professorList; // 학과교수 리스트
 	private List<Student> studentList; // 학생 리스트
-	private int maxProfessor;	//교수 정원 제한
-	private int maxStudent;		//학생 정원 제한
+	private int maxProfessor;	//교수 정원 제한(5 고정)
+	private int maxStudent;		//학생 정원 제한(10 고정)
 	
 	//수정, 삭제시 사용
 	@Override
@@ -39,10 +39,11 @@ public class Department {
 		return Objects.equals(name, other.name);
 	}
 	
-	public Department(String name, int maxProfessor, int maxStudent) {
-		this.name = name;
-		this.maxProfessor = maxProfessor;
-		this.maxStudent = maxStudent;
-	}
 	
+	//학과 등록 생성자 : 학과 이름		*교수 정원(5 고정), 학생 정원(10 고정)
+	public Department(String name) {
+		this.name = name;
+		this.maxStudent = 10;
+		this.maxProfessor = 5;
+	}
 }

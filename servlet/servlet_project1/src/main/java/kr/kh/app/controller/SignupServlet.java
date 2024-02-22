1e342ebc34111b06000cc5d10ec4b5db06f5a352
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2024-02-22 17:05:33 +0900

    서블릿 1일차 : 회원가입, 로그인 기능 구현 완료

--- servlet/servlet_project1/src/main/java/kr/kh/app/controller/SignupServlet.java
+++ servlet/servlet_project1/src/main/java/kr/kh/app/controller/SignupServlet.java
@@ -1,19 +1,25 @@
 package kr.kh.app.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.kh.app.model.vo.MemberVO;
+import kr.kh.app.service.MemberService;
+import kr.kh.app.service.MemberServiceImp;
+
 
 @WebServlet("/signup")
 public class SignupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-   
+	private MemberService memberService = new MemberServiceImp();
+	
     public SignupServlet() {
+    	
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -23,13 +29,14 @@ public class SignupServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
-		String pw2 = request.getParameter("pw2");
 		String email = request.getParameter("email");
-		System.out.println("아이디 : " + id);
-		System.out.println("비밀번호 : " + pw);
-		System.out.println("비밀번호 확인 : " + pw2);
-		System.out.println("이메일 : " + email);
-		doGet(request, response);
+		if(memberService.signup(new MemberVO(id, pw, email))) {
+			//회원가입에 성공하면 메인 페이지로 이동
+			response.sendRedirect(request.getContextPath()+"/");
+		}else {
+			//실패하면 회원가입 페이지 유지
+			doGet(request, response);			
+		}
 	}
 
 }

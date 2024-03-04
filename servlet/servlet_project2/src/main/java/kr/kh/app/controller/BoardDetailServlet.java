e8ab792a32d5ebff8650b5409ff877e4ef6f772d
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2024-03-04 09:35:28 +0900

    서블릿 6일차 : 서블릿 복습 - 게시글 삭제 구현 완료

--- servlet/servlet_project2/src/main/java/kr/kh/app/controller/BoardDetailServlet.java
+++ servlet/servlet_project2/src/main/java/kr/kh/app/controller/BoardDetailServlet.java
@@ -13,7 +13,7 @@ import kr.kh.app.service.BoardService;
 import kr.kh.app.service.BoardServiceImp;
 
 @WebServlet("/board/detail")
-public class DetailBoardServlet extends HttpServlet {
+public class BoardDetailServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private BoardService boardService = new BoardServiceImp();
 	

391be6c1a1827d60b23b72bb13b0c048a44f9df0
seungjoo555 <153248378+seungjoo555@users.noreply.github.com>
2024-03-12 11:18:24 +0900

    스프링 1일차: 스프링 프로젝트 생성 및 인코딩 설정 완료

---
+++ spring/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -0,0 +1,22 @@
+package kr.kh.spring.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	//value : url, method : 전송방식(get, post)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		
+		return "home";
+	}
+	
+}
